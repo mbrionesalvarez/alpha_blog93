a9c8e1a7be0f9799d005523319cdede4c05e1ff4
Marcos Briones <brionesalvarezmarcos@gmail.com>
2020-04-04 21:19:15 +0000

    Add secure password system 134

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_234056) do
+ActiveRecord::Schema.define(version: 2020_04_04_171252) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_04_03_234056) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
